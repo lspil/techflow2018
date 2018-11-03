5d546e609263467741c74696f1994bfbe3c63fb3
Laurentiu Spilca <laurentiu.spilca@endava.com>
2018-11-03 20:43:33 +0200

    Add hello project

---
+++ techflowdemo/src/main/java/com/endava/techflowdemo/TechflowdemoApplication.java
@@ -0,0 +1,12 @@
+package com.endava.techflowdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechflowdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechflowdemoApplication.class, args);
+	}
+}
